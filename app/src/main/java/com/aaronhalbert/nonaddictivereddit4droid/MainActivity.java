83e9d45f9101cf6502625ebae4c9cb4ad38c4291
Aaron Halbert <ajh3@protonmail.com>
2018-07-27 16:47:24 -0700

    viewpager with materialtabs and dummy fragments

--- app/src/main/java/com/aaronhalbert/nonaddictivereddit4droid/MainActivity.java
+++ app/src/main/java/com/aaronhalbert/nonaddictivereddit4droid/MainActivity.java
@@ -1,13 +1,26 @@
 package com.aaronhalbert.nonaddictivereddit4droid;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.karim.MaterialTabs;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewPager pager;
+    private RedditFragmentPagerAdapter adapter;
+    private MaterialTabs tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.pager);
+        tabs = findViewById(R.id.tabs);
+
+        pager.setAdapter(new RedditFragmentPagerAdapter(getSupportFragmentManager()));
+        tabs.setViewPager(pager);
+
     }
 }
---
+++ app/src/main/java/com/aaronhalbert/nonaddictivereddit4droid/RedditFragmentPagerAdapter.java
@@ -0,0 +1,25 @@
+package com.aaronhalbert.nonaddictivereddit4droid;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class RedditFragmentPagerAdapter extends FragmentPagerAdapter {
+    static final int NUM_ITEMS = 2;
+
+    public RedditFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return AllPostsFragment.newInstance("abc", "def");
+    }
+
+
+}
