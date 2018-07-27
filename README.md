89d2cdb656bf2ca9a71b83016689ea7c9bab3cca
Aaron Halbert <ajh3@protonmail.com>
2018-07-27 12:36:52 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NonAddictiveReddit4Droid
---
+++ app/src/main/java/com/aaronhalbert/nonaddictivereddit4droid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aaronhalbert.nonaddictivereddit4droid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
