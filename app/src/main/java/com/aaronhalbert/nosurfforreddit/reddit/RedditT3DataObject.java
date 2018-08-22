cf4c56fb79263f662019f793389e2b527d2253f8
Aaron Halbert <ajh3@protonmail.com>
2018-08-22 14:31:30 -0700

    update Gson model for selfposts

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/reddit/RedditT3DataObject.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/reddit/RedditT3DataObject.java
@@ -1,10 +1,13 @@
 package com.aaronhalbert.nosurfforreddit.reddit;
 
+import com.google.gson.annotations.SerializedName;
+
 public class RedditT3DataObject {
     private String subreddit;
     private String title;
     private String thumbnail;
     private int score;
+    @SerializedName("is_self") private boolean isSelf;
 
     public String getSubreddit() {
         return subreddit;
@@ -17,4 +20,6 @@ public class RedditT3DataObject {
     public String getThumbnail() { return thumbnail; }
 
     public int getScore() { return score; }
+
+    public boolean getIsSelf() { return isSelf; }
 }
