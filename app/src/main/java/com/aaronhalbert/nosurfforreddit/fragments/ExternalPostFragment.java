7daa5413a20e2d39f68bd1f3f9c6df9919a198ab
Aaron Halbert <ajh3@protonmail.com>
2018-08-25 22:31:38 -0700

    implement external post loading in WebView

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/ExternalPostFragment.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/ExternalPostFragment.java
@@ -20,14 +20,9 @@ import com.aaronhalbert.nosurfforreddit.R;
  * create an instance of this fragment.
  */
 public class ExternalPostFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String KEY_URL = "url";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String url;
 
     private WebView browser;
 
@@ -37,20 +32,10 @@ public class ExternalPostFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExternalPostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ExternalPostFragment newInstance(String param1, String param2) {
+    public static ExternalPostFragment newInstance(String url) {
         ExternalPostFragment fragment = new ExternalPostFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(KEY_URL, url);
         fragment.setArguments(args);
         return fragment;
     }
@@ -59,8 +44,7 @@ public class ExternalPostFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            url = getArguments().getString(KEY_URL);
         }
     }
 
@@ -72,7 +56,7 @@ public class ExternalPostFragment extends Fragment {
 
         browser = result.findViewById(R.id.webkit);
         browser.getSettings().setJavaScriptEnabled(true);
-        browser.loadUrl("https://www.google.com");
+        browser.loadUrl(url);
 
         return result;
     }
