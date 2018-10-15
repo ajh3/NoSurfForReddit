594d8bcb20d9b3f9cca4d639e959aa62765235be
Aaron Halbert <ajh3@protonmail.com>
2018-10-14 17:04:53 -0700

    eliminate redundant binding adapter for thumbnail image

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/DataBindingHandlers.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/DataBindingHandlers.java
@@ -5,17 +5,8 @@ import android.widget.ImageView;
 
 public class DataBindingHandlers {
     //binding framework calls this any time it finds an ImageView with an imageUrl synthetic property
-    @BindingAdapter("app:imageUrl")
+    @BindingAdapter("imageUrl")
     public static void bindImage(ImageView iv, String url) {
-
-        GlideApp.with(iv.getContext())
-                .load(url)
-                .centerCrop()
-                .into(iv);
-    }
-
-    @BindingAdapter("app:thumbnailUrl")
-    public static void bindThumbnail(ImageView iv, String url) {
 
         GlideApp.with(iv.getContext())
                 .load(url)
