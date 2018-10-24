384957bbccfb01b6e47b2303733197adbb90b92f
Aaron Halbert <ajh3@protonmail.com>
2018-10-23 21:20:03 -0700

    refactor ReadPostId

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/room/ReadPostId.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/room/ReadPostId.java
@@ -4,19 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-/**
- * A basic class representing an entity that is a row in a one-column database table.
- *
- * @ Entity - You must annotate the class as an entity and supply a table name if not class name.
- * @ PrimaryKey - You must identify the primary key.
- * @ ColumnInfo - You must supply the column name if it is different from the variable name.
- *
- * See the documentation for the full rich set of annotations.
- * https://developer.android.com/topic/libraries/architecture/room.html
- */
-
 @Entity(tableName = "read_post_id_table")
 public class ReadPostId {
+
     @PrimaryKey //this field is its own primary key
     @NonNull
     private String readPostId;
@@ -28,8 +18,4 @@ public class ReadPostId {
     public String getReadPostId() {
         return this.readPostId;
     }
-
 }
-
-
-
