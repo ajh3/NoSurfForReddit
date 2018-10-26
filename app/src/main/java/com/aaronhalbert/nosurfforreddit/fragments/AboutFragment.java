c7c1d4436696973b73997d4a442ce851af8f50a0
Aaron Halbert <ajh3@protonmail.com>
2018-10-26 13:36:14 -0700

    refactor AboutFragment

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/AboutFragment.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/AboutFragment.java
@@ -9,21 +9,14 @@ import android.view.ViewGroup;
 import com.aaronhalbert.nosurfforreddit.R;
 
 public class AboutFragment extends BaseFragment {
-
     public static AboutFragment newInstance() {
         AboutFragment fragment = new AboutFragment();
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
