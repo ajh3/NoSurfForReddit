1c24926673804e29665155843880c546cd919b78
Aaron Halbert <ajh3@protonmail.com>
2018-10-26 13:52:19 -0700

    reformat LinkPostFragment

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/LinkPostFragment.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/LinkPostFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 public class LinkPostFragment extends PostFragment {
     public static LinkPostFragment newInstance(int position, boolean isSubscribedPost) {
         LinkPostFragment fragment = new LinkPostFragment();
+        //TODO: store this state in VM instead of passing to fragment and storing in bundle?
         Bundle args = new Bundle();
         args.putInt(KEY_POSITION, position);
         args.putBoolean(KEY_IS_SUBSCRIBED_POST, isSubscribedPost);
