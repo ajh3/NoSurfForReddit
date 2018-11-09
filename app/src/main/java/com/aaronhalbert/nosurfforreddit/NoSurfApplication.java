06d8cb85669b33e2ff1edfe40a761af9ed5a04cf
Aaron Halbert <ajh3@protonmail.com>
2018-11-09 00:16:27 -0800

    add LeakCanary

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/NoSurfApplication.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/NoSurfApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.aaronhalbert.nosurfforreddit.dependencyinjection.application.ApplicationComponent;
 import com.aaronhalbert.nosurfforreddit.dependencyinjection.application.ApplicationModule;
 import com.aaronhalbert.nosurfforreddit.dependencyinjection.application.DaggerApplicationComponent;
+import com.squareup.leakcanary.LeakCanary;
 
 public class NoSurfApplication extends Application {
     private ApplicationComponent applicationComponent;
@@ -12,6 +13,13 @@ public class NoSurfApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         applicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
