76cef4056eb61baba15de8e1edda0a593c453164
Aaron Halbert <ajh3@protonmail.com>
2018-11-10 21:11:37 -0800

    refactor PostFragment

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/PostFragment.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/PostFragment.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 
 import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,18 +46,18 @@ abstract public class PostFragment extends BaseFragment {
 
         /* avoid additional comments network call on config change
          *
-         * Could wrap the comments in an Event wrapper and handle it like navigation events,
-         * but I'm not using saved instance state for much else, so let's use the Bundle here
-         * just to mix things up */
+         * Could wrap the comments in an Event wrapper and handle it similar to navigation events,
+         * but I'm not using saved instance state for much else, so let's use a Bundle here
+         * just for fun */
         if (savedInstanceState != null) {
-            commentsAlreadyLoaded = savedInstanceState.getBoolean(KEY_COMMENTS_ALREADY_LOADED, false);
+            commentsAlreadyLoaded = savedInstanceState
+                    .getBoolean(KEY_COMMENTS_ALREADY_LOADED, false);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         setupBinding(container);
         findPostViews();
         setupPostViews();
@@ -79,7 +80,10 @@ abstract public class PostFragment extends BaseFragment {
     public void onImageClick(View view) {
         String url = postsViewStateLiveData.getValue().postData.get(lastClickedPostPosition).url;
 
-        viewModel.setPostFragmentClickEventsLiveData(new LaunchWebViewParams(url, null, false));
+        viewModel.setPostFragmentClickEventsLiveData(new LaunchWebViewParams(
+                url,
+                null,
+                false));
     }
 
     // endregion listeners -------------------------------------------------------------------------
@@ -105,13 +109,17 @@ abstract public class PostFragment extends BaseFragment {
     }
 
     private void findPostViews() {
+        // put these views into arrays to more easily work with them in for loops
         comments[0] = fragmentPostBinding.postFragmentFirstComment;
         comments[1] = fragmentPostBinding.postFragmentSecondComment;
         comments[2] = fragmentPostBinding.postFragmentThirdComment;
 
-        comments[0].setMovementMethod(LinkMovementMethod.getInstance());
-        comments[1].setMovementMethod(LinkMovementMethod.getInstance());
-        comments[2].setMovementMethod(LinkMovementMethod.getInstance());
+        // setting a LinkMovementMethod on the comments fields makes links clickable
+        MovementMethod m = LinkMovementMethod.getInstance();
+
+        comments[0].setMovementMethod(m);
+        comments[1].setMovementMethod(m);
+        comments[2].setMovementMethod(m);
 
         commentsDetails[0] = fragmentPostBinding.postFragmentFirstCommentDetails;
         commentsDetails[1] = fragmentPostBinding.postFragmentSecondCommentDetails;
@@ -132,6 +140,10 @@ abstract public class PostFragment extends BaseFragment {
     }
 
     private void setupComments() {
+        /* To get the comments for a given post, we have to take the post's ID and make a separate
+         * API call, which happens here, and we record that the post has been clicked in order
+         * to now display it with struck-out / grayed-out formatting */
+
         if (!commentsAlreadyLoaded) {
             observeCommentsFinishedLoadingLiveEvent();
             viewModel.fetchPostCommentsASync(lastClickedPostId);
@@ -142,6 +154,8 @@ abstract public class PostFragment extends BaseFragment {
     }
 
     private void updateViewVisibilities() {
+        /* show the correct comment and divider views based on how many comments the post has */
+
         int numComments = viewModel.getCommentsViewStateLiveData().getValue().numComments;
 
         for (int i = 0; i < numComments; i++) {
