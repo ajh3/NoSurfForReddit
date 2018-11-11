78b35082a7de2a24c20682333131128d05e26e8c
Aaron Halbert <ajh3@protonmail.com>
2018-11-10 21:27:24 -0800

    comment AllPostsFragment and SubscribedPostsFragment

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/AllPostsFragment.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/AllPostsFragment.java
@@ -2,6 +2,13 @@ package com.aaronhalbert.nosurfforreddit.fragments;
 
 import com.aaronhalbert.nosurfforreddit.adapters.PostsAdapter;
 
+/* for the first page of the RecyclerView - posts from r/all
+ *
+ * search for "subscribedposts" globally in this project for more comments about allposts
+ * vs. subscribedposts
+ *
+ * also see SubscribedPostsFragment */
+
 public class AllPostsFragment extends PostsFragment {
     public static AllPostsFragment newInstance() {
         return new AllPostsFragment();
--- app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/SubscribedPostsFragment.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/fragments/SubscribedPostsFragment.java
@@ -2,6 +2,15 @@ package com.aaronhalbert.nosurfforreddit.fragments;
 
 import com.aaronhalbert.nosurfforreddit.adapters.PostsAdapter;
 
+/* for the second page of the RecyclerView - posts from the user's subscribed subreddits
+ *
+ * only used when user is logged in
+ *
+ * search for "subscribedposts" globally in this project for more comments about allposts
+ * vs. subscribedposts
+ *
+ * also see AllPostsFragment*/
+
 public class SubscribedPostsFragment extends PostsFragment {
 
     public static SubscribedPostsFragment newInstance() {
