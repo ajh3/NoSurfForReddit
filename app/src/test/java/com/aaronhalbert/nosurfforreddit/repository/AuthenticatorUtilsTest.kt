191e96211eda803aa0f1e0f120aa149e437c5c57
Aaron Halbert <ajh3@protonmail.com>
2019-01-02 23:04:12 -0700

    refactor AuthenticatorUtilsTest

--- app/src/test/java/com/aaronhalbert/nosurfforreddit/repository/AuthenticatorUtilsTest.kt
+++ app/src/test/java/com/aaronhalbert/nosurfforreddit/repository/AuthenticatorUtilsTest.kt
@@ -5,6 +5,8 @@ import android.net.Uri
 import com.nhaarman.mockitokotlin2.KStubbing
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import org.hamcrest.CoreMatchers.endsWith
+import org.hamcrest.CoreMatchers.startsWith
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -21,12 +23,19 @@ class AuthenticatorUtilsTest {
         sut = AuthenticatorUtils()
     }
 
+    /* note that buildAuthUrl() comprises no logic of my own, it's just calls to 3rd-party static
+     * methods. So, this test is effectively testing code that is not my own. However, it's still
+     * useful, because it'll warn us if any of the constants used to build the URL
+     * accidentally get changed.
+     *
+     * Also note that this test works by asserting the beginning and end substrings of the URL:
+     * what's in the middle is simply a randomly-generated UUID, which there is no need to test. */
     @Test
     fun buildAuthUrl_success_stringMatches() {
-        //TODO: move buildAuthUrl() out of AuthenticatorUtils, so the class can be fully tested?
-        /* this function comprises only static calls to classes I don't own - no reason to test,
-         * as if I wrap the static calls in wrapper classes and mock them, I'll just be testing
-         * the mocks. */
+        val result = sut.buildAuthUrl()
+
+        assertThat(result, startsWith("https://www.reddit.com/api%2Fv1%2Fauthorize.compact?client_id=jPF59UF5MbMkWg&response_type=code&state="))
+        assertThat(result, endsWith("&redirect_uri=nosurfforreddit%3A%2F%2Foauth&duration=permanent&scope=identity%20mysubreddits%20read"))
     }
 
     @Test
