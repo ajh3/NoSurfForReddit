6e0275363a5b954bf530e46ec378d3404e8226bc
Aaron Halbert <ajh3@protonmail.com>
2019-01-08 13:28:29 -0700

    comment ClickedPostIdDao

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/data/local/clickedpostids/ClickedPostIdDao.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/data/local/clickedpostids/ClickedPostIdDao.java
@@ -26,15 +26,15 @@ import io.reactivex.Observable;
 @Dao
 public interface ClickedPostIdDao {
 
-    //specifying the return type is all that's needed, Room does the rest
+    //Note to self: specifying the return type is all that's needed, Room does the rest
     @Query("SELECT * from clicked_post_id_table")
     Observable<List<ClickedPostId>> getAllClickedPostIds();
 
-    // this is a convenience annotation, not necessary to write any SQL for a basic insert
+    //Note to self:  this is a convenience annotation, not necessary to write any SQL for a basic insert
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertClickedPostId(ClickedPostId clickedPostId);
 
-    // DELETE FROM with no WHERE clause deletes all records in the table
+    //Note to self:  DELETE FROM with no WHERE clause deletes all records in the table
     @SuppressWarnings("unused")
     @Query("DELETE FROM clicked_post_id_table")
     void deleteAllClickedPostIds();
