0c2d444e76c8f4e12535d447eed9bfcb2487820d
Aaron Halbert <ajh3@protonmail.com>
2019-12-11 10:01:11 -0700

    update dependencies

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/NoSurfApplication.java
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/NoSurfApplication.java
@@ -18,7 +18,6 @@ import android.app.Application;
 import com.aaronhalbert.nosurfforreddit.di.application.ApplicationComponent;
 import com.aaronhalbert.nosurfforreddit.di.application.ApplicationModule;
 import com.aaronhalbert.nosurfforreddit.di.application.DaggerApplicationComponent;
-import com.squareup.leakcanary.LeakCanary;
 
 public class NoSurfApplication extends Application {
     private ApplicationComponent applicationComponent;
@@ -27,20 +26,9 @@ public class NoSurfApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        initLeakCanary();
-
         applicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
                 .build();
-    }
-
-    private void initLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
     }
 
     @SuppressWarnings("UnusedReturnValue")
