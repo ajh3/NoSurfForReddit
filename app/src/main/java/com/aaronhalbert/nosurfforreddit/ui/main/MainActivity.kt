33e248e8dd1a790895569e5b77a15518958155f0
Aaron Halbert <ajh3@protonmail.com>
2020-02-10 13:28:36 -0800

    remove unused imports

--- app/src/main/java/com/aaronhalbert/nosurfforreddit/ui/main/MainActivity.kt
+++ app/src/main/java/com/aaronhalbert/nosurfforreddit/ui/main/MainActivity.kt
@@ -19,7 +19,6 @@ import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
